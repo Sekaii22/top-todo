4630c9bdf7cf279c7b8fd0a7320d29e54f058c50
Sekaii22 <shawn.ng22@gmail.com>
2025-05-28 08:05:48 +0800

    Add styling to sidebar and collapsible toggle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import "./styles.css";
 
-alert("Hello world");
+const sidebarBtn = document.querySelector(".sidebar-btn");
+const body = document.querySelector("body");
+const sidebarHeader = document.querySelector(".sidebar header");
+const sidebarNav = document.querySelector(".sidebar nav");
+
+sidebarBtn.addEventListener("click", () => {
+    body.classList.toggle("collapse-grid");
+    sidebarNav.classList.toggle("collapse");
+    sidebarHeader.classList.toggle("collapse");
+});
